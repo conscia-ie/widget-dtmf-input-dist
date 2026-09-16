5aab7bf25fb58a7eab17294a798aafc1205cf820
Stephen Gallagher <sgal@conscia.com>
2026-09-16 16:20:56 +0100

    fix: dark mode icon color

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ In order to be iterable, non-array objects must have a [Symbol.iterator]() metho
            ========================================================= */
 
         :host {
-          margin-top: 20px;
+          margin-top: 18px;
 
           display: flex !important;
           align-items: center;
